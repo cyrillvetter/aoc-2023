c9a10268428dab46ac425f93723b15ef27940187
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-02 11:49:42 +0100

    day 02 improvements

--- haskell/Day02.hs
+++ haskell/Day02.hs
@@ -1,18 +1,15 @@
 import Data.List.Split (splitOn)
 
-data Color = Blue | Red | Green deriving (Enum, Eq)
-
 main = do
-    input <- map parse . lines <$> readFile "inputs/2.txt"
-    print $ sum $ map fst $ filter (isValidGame . concat . snd) input
-    print $ sum $ map (getGamePower . concat . snd) input
-    print "Day 2"
+    input <- zip [1..] . map parse . lines <$> readFile "inputs/2.txt"
+    print $ sum $ map fst $ filter (isValidGame . snd) input
+    print $ sum $ map (getGamePower . snd) input
 
 isValidGame :: [(Int, String)] -> Bool
-isValidGame g = has 14 "blue" && has 12 "red" && has 13 "green"
+isValidGame g = hasLessThan 14 "blue" && hasLessThan 12 "red" && hasLessThan 13 "green"
     where
-          has :: Int -> String -> Bool
-          has n c = all ((<= n) . fst) $ filter ((== c) . snd) g
+        hasLessThan :: Int -> String -> Bool
+        hasLessThan n c = all ((<= n) . fst) $ filter ((== c) . snd) g
 
 getGamePower :: [(Int, String)] -> Int
 getGamePower g = maxOf "blue" * maxOf "red" * maxOf "green"
@@ -20,12 +17,8 @@ getGamePower g = maxOf "blue" * maxOf "red" * maxOf "green"
         maxOf :: String -> Int
         maxOf c = maximum $ map fst $ filter ((== c) . snd) g
 
-parse :: String -> (Int, [[(Int, String)]])
-parse l = (gameNum, games)
-    where [numPart, rest] = splitOn ": " l
-          gameNum = read $ last $ words numPart
-          games = map (parseGame . splitOn ", ") $ splitOn "; " rest
-
-parseGame :: [String] -> [(Int, String)]
-parseGame = map p
-    where p = (\[num, col] -> (read num, col)) . words
+parse :: String -> [(Int, String)]
+parse l = games
+    where [_, g] = splitOn ": " l
+          splitGames = concatMap (splitOn ", ") $ splitOn "; " g
+          games = map ((\[num, col] -> (read num, col)) . words) splitGames
