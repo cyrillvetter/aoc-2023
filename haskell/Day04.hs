5fd1f80d37c40599efeb8d75d290023fdf4c625d
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-04 11:30:04 +0100

    minor improvements to day 4

--- haskell/Day04.hs
+++ haskell/Day04.hs
@@ -4,30 +4,19 @@ import qualified Data.IntMap as M
 
 main = do
     input <- map parse . lines <$> readFile "inputs/4.txt"
-    print $ sum $ map (uncurry calcCardPoints) input
+    print $ sum $ map calcCardPoints input
+    print $ calcTotalCards 1 input $ M.fromList $ map (\l -> (l, 1)) [1..length input]
 
-    let mine = map fst input
-        winning = map snd input
-        cards = M.fromList $ map (\l -> (l, 1)) [1..length input]
-    print $ calcTotalCards 1 mine winning cards
+calcCardPoints :: Int -> Int
+calcCardPoints n = if n == 0 then 0 else 2 ^ (n - 1)
 
-calcCardPoints :: [Int] -> [Int] -> Int
-calcCardPoints mine winning
-    | matches == 0 = 0
-    | otherwise = 2 ^ (matches - 1)
-    where matches = length (mine `intersect` winning)
+calcTotalCards :: Int -> [Int] -> M.IntMap Int -> Int
+calcTotalCards _ [] cards = sum $ M.elems cards
+calcTotalCards currCard (x:xs) cards = calcTotalCards (currCard + 1) xs nextCards
+    where amount = cards M.! currCard
+          copies = take x [currCard+1..]
+          nextCards = foldl (flip $ M.adjust (+ amount)) cards copies
 
-calcTotalCards :: Int -> [[Int]] -> [[Int]] -> M.IntMap Int -> Int
-calcTotalCards _ [] [] cards = sum $ M.elems cards
-calcTotalCards currCard (m:ms) (w:ws) cards = calcTotalCards (currCard + 1) ms ws adjusted
-    where cardsAmount = cards M.! currCard
-          matches = length (m `intersect` w)
-          copies = take matches [currCard+1..]
-          adjusted = foldl (adjustCards cardsAmount) cards copies
-
-adjustCards :: Int -> M.IntMap Int -> Int -> M.IntMap Int
-adjustCards amount cards cardNum = M.adjust (+ amount) cardNum cards
-
-parse :: String -> ([Int], [Int])
-parse l = (mine, winning)
-    where [mine, winning] = map (map read . words) $ splitOn " | " $ last $ splitOn ": " l
+parse :: String -> Int
+parse l = length $ intersect mine winning
+    where [mine, winning] = map words $ splitOn " | " $ last $ splitOn ": " l
