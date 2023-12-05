a5b34f30028376f9f50a46ac126888f0b58a6baa
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-05 08:06:28 +0100

    slow solution for day 5

--- haskell/Day05.hs
+++ haskell/Day05.hs
@@ -1,3 +1,32 @@
+import Data.List.Split (splitOn, chunksOf)
+
+type Mapping = (Int, Int, Int)
+
 main = do
-    input <- readFile "inputs/5.txt"
-    putStrLn "Day 5"
+    input <- splitOn "\n\n" <$> readFile "inputs/5.txt"
+    let seeds = parseSeeds $ head input
+        maps = map parseMap $ drop 1 input
+        seedRanges = map (\[a, b] -> (a, a + b)) $ chunksOf 2 seeds
+    print $ minimum $ map (`mapToLocation` maps) seeds
+    print $ minimum $ concatMap (\(from, to) -> mapAll from to maps) seedRanges -- ~3min execution time
+
+mapToLocation :: Int -> [[Mapping]] -> Int
+mapToLocation curr [] = curr
+mapToLocation curr (m:ms) = mapToLocation (check m) ms
+    where
+        check :: [Mapping] -> Int
+        check [] = curr
+        check ((dest, source, range):xs)
+            | curr >= source && curr < (source + range) = (curr - source) + dest
+            | otherwise = check xs
+
+mapAll :: Int -> Int -> [[Mapping]] -> [Int]
+mapAll curr to maps
+    | curr == to = []
+    | otherwise = mapToLocation curr maps : mapAll (curr + 1) to maps
+
+parseMap :: String -> [Mapping]
+parseMap l = map ((\[a, b, c] -> (read a, read b, read c)) . words) $ drop 1 $ lines l
+
+parseSeeds :: String -> [Int]
+parseSeeds = map read . words . last . splitOn ": "
