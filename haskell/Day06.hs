f7ab7fd831a11e97cd1ca79c1cc7d7f97e6b6543
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-06 14:25:15 +0100

    improvements

--- haskell/Day06.hs
+++ haskell/Day06.hs
@@ -1,12 +1,10 @@
-import Data.List.Split (splitOn)
 import Data.List (transpose)
-import Common (toTuple)
 
 main = do
-    input <- map (words . last . splitOn ":") . lines <$> readFile "inputs/6.txt"
-    print $ product $ map (countRecords . toTuple . map read) $ transpose input
-    print $ countRecords $ toTuple $ map (read . concat) input
+    input <- map (drop 1 . words) . lines <$> readFile "inputs/6.txt"
+    print $ product $ map (countRecords . map read) $ transpose input
+    print $ countRecords $ map (read . concat) input
 
-countRecords :: (Int, Int) -> Int
-countRecords (time, dist) = time - (2 * untilRecord) + 1
+countRecords :: [Int] -> Int
+countRecords [time, dist] = time - (2 * untilRecord) + 1
     where untilRecord = head $ dropWhile (\charge -> (time - charge) * charge <= dist) [1..]
