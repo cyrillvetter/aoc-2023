e7f7edb2e3380aa1dbc84bcdd8a69db3a86a5425
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-07 18:26:20 +0100

    day 7 minor improvements

--- haskell/Day07.hs
+++ haskell/Day07.hs
@@ -1,24 +1,24 @@
 import Data.List (sort, sortBy, sortOn, group)
-import qualified Data.Map as M
+import Data.Map ((!), fromList)
 
-strengths = M.fromList $ zip "AKQJT98765432" [12,11..]
-jokerStrengths = M.fromList $ zip "AKQT98765432J" [12,11..]
+strengths = fromList $ zip "AKQJT98765432" [12,11..]
+jokerStrengths = fromList $ zip "AKQT98765432J" [12,11..]
 
-type Hand = (String, Int, Int, [Int])
+type Hand = (Int, Int, [Int])
 
 main = do
     input <- map words . lines <$> readFile "inputs/7.txt"
-    let handTypes = map (\[h, b] -> (h, read b, getHandType h, map (strengths M.!) h)) input
-        jokerHandTypes = map (\[h, b] -> (h, read b, getJokerHandType h, map (jokerStrengths M.!) h)) input
+    let handTypes = map (\[h, b] -> (read b, getHandType h, map (strengths !) h)) input
+        jokerHandTypes = map (\[h, b] -> (read b, getJokerHandType h, map (jokerStrengths !) h)) input
 
     print $ calcTotalWinnings handTypes
     print $ calcTotalWinnings jokerHandTypes
 
 calcTotalWinnings :: [Hand] -> Int
-calcTotalWinnings s = sum $ zipWith (*) [1..] $ map (\(_, b, _, _) -> b) $ sortBy compareHand s
+calcTotalWinnings s = sum $ zipWith (*) [1..] $ map (\(b, _, _) -> b) $ sortBy compareHand s
 
 compareHand :: Hand -> Hand -> Ordering
-compareHand (_, _, t1, s1) (_, _, t2, s2)
+compareHand (_, t1, s1) (_, t2, s2)
     | primary == EQ = compare s1 s2
     | otherwise = primary
     where primary = compare t1 t2
@@ -34,7 +34,6 @@ getHandType s = case sort $ map length $ group $ sort s of
     _            -> 0
 
 getJokerHandType :: String -> Int
-getJokerHandType s
-    | s == "JJJJJ" = 6
-    | otherwise = getHandType $ map (\c -> if c == 'J' then mostCommon else c) s
+getJokerHandType "JJJJJ" = 6
+getJokerHandType s = getHandType $ map (\c -> if c == 'J' then mostCommon else c) s
     where mostCommon = head $ last $ sortOn length $ group $ sort $ filter (/= 'J') s
