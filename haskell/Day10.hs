c2e8c7b4ecccda6b437e698d2f9d8c27fb52e035
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-10 20:27:25 +0100

    start day 10 part 2 implementation

--- haskell/Day10.hs
+++ haskell/Day10.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.List (intercalate, transpose)
 
 type Point = (Int, Int)
 
@@ -8,18 +10,39 @@ left = (-1, 0)
 right = (1, 0)
 
 main = do
-    input <- createGrid . lines <$> readFile "inputs/10.txt"
-    let grid = M.fromList input
-        start = fst $ head $ filter ((== 'S') . snd) input
-        startPipes = map (addPoints start) [up, right, down, left]
-        connectedPipe = head $ filter (\p -> start `elem` getNeighbours p (grid M.! p)) startPipes
+    -- input <- createGrid . lines <$> readFile "inputs/10.txt"
+    input <- lines <$> readFile "inputs/10.txt"
+    let grid = createGrid input
+        pipeMap = M.fromList grid
+        start = fst $ head $ filter ((== 'S') . snd) grid
+        startNeighbourPipes = map (addPoints start) [up, right, down, left]
+        connectedPipe = head $ filter (\p -> start `elem` getNeighbours p (pipeMap M.! p)) startNeighbourPipes
+        notConnectedPipe = head $ filter (\p -> start `notElem` getNeighbours p (pipeMap M.! p)) startNeighbourPipes
 
-    print $ getMainLoopLength start connectedPipe 0 grid
+    print notConnectedPipe
+    print $ getMainLoop start connectedPipe 0 pipeMap
+    putStrLn $ intercalate "\n" $ scale input
 
-getMainLoopLength :: Point -> Point -> Int -> M.Map Point Char -> Int
-getMainLoopLength prev curr steps pipes
+scale :: [[Char]] -> [[Char]]
+scale = transpose . map (addBefore upReplace) . transpose . map (addBefore leftReplace)
+    where
+        addBefore :: (Char -> Char) -> [Char] -> [Char]
+        addBefore f = concatMap (\c -> [f c, c])
+
+upReplace :: Char -> Char
+upReplace c
+    | c == '|' || c == 'L' || c == 'J' = '|'
+    | otherwise = '.'
+
+leftReplace :: Char -> Char
+leftReplace c
+    | c == '-' || c == 'J' || c == '7' = '-'
+    | otherwise = '.'
+
+getMainLoop :: Point -> Point -> Int -> M.Map Point Char -> Int
+getMainLoop prev curr steps pipes
     | currPipe == 'S' = (steps `div` 2) + 1
-    | otherwise = getMainLoopLength curr next (steps + 1) pipes
+    | otherwise = getMainLoop curr next (steps + 1) pipes
     where currPipe = pipes M.! curr
           next = head $ filter (/= prev) $ getNeighbours curr currPipe
 
