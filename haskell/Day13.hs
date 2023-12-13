d2cbeb915193908082945b989badf83ae11de132
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-13 20:42:15 +0100

    use splitAt instead of drop and take

--- haskell/Day13.hs
+++ haskell/Day13.hs
@@ -1,6 +1,6 @@
 import Data.List.Split (splitOn)
 import Data.List (transpose, find)
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Maybe (fromJust, fromMaybe, mapMaybe)
 
 main = do
     input <- map lines . splitOn "\n\n" <$> readFile "inputs/13.txt"
@@ -9,19 +9,17 @@ main = do
 
 getReflectionValue :: Int -> [String] -> Int
 getReflectionValue smudges mirror = fromMaybe rowReflection colReflection
-    where cols = transpose mirror
-          colReflection = getReflectionIndex smudges cols
+    where colReflection = getReflectionIndex smudges $ transpose mirror
           rowReflection = 100 * fromJust (getReflectionIndex smudges mirror)
 
 getReflectionIndex :: Int -> [String] -> Maybe Int
 getReflectionIndex smudges mirror = (\(i, _, _) -> i) <$> find isReflective zipped
     where zipped = zip3 [1..] mirror $ tail mirror
-          isReflective (i, to, from) = countDifferences to from <= smudges && countReflectiveDifferences i mirror == smudges
+          isReflective (i, to, from) = countDifferences to from <= smudges && countSmudges i mirror == smudges
 
-countReflectiveDifferences :: Int -> [String] -> Int
-countReflectiveDifferences i l = sum $ zipWith countDifferences to from
-    where to = drop i l
-          from = reverse $ take i l
+countSmudges :: Int -> [String] -> Int
+countSmudges i l = sum $ zipWith countDifferences (reverse from) to
+    where (from, to) = splitAt i l
 
 countDifferences :: String -> String -> Int
 countDifferences x y = length $ filter (uncurry (/=)) $ zip x y
