3e034d02cb4f91eae2c9e17f3682ba66cf15bc16
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-14 10:33:02 +0100

    day 14 part 1

--- haskell/Day14.hs
+++ haskell/Day14.hs
@@ -1,17 +1,14 @@
-import Data.List (transpose, intercalate)
-import Debug.Trace (trace)
+import Data.List (transpose)
 
 main = do
     input <- transpose . lines <$> readFile "inputs/14.txt"
-    putStrLn $ intercalate "\n" input
-    print $ map (\w -> calcRowWeight w (length input)) input
+    print $ sum $ map (calcWeight (length input)) input
 
-calcRowWeight :: String -> Int -> Int
-calcRowWeight row width = calcWeight (zip [width,width-1..] row) width
-    where
-        calcWeight :: [(Int, Char)] -> Int -> Int
-        calcWeight n curr
-            | null toEnd = sum $ take rocksAmount [curr,curr-1..]
-            | otherwise = sum (take rocksAmount [curr,curr-1..]) + calcWeight (tail toEnd) (fst (head toEnd))
-            where (until, toEnd) = span ((/= '#') . snd) n
-                  rocksAmount = length $ filter ((== 'O') . snd) until
+calcWeight :: Int -> String -> Int
+calcWeight solid row
+    | null toEnd = currSum
+    | otherwise = currSum + calcWeight nextSolid (tail toEnd)
+    where (until, toEnd) = span (/= '#') row
+          nextSolid = solid - length until - 1
+          rocksAmount = length $ filter (== 'O') until
+          currSum = sum [solid,solid-1..solid-rocksAmount+1]
