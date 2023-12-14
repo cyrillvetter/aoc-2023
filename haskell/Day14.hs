092fb9339b2d6988b2bdeb10b9f42291a4f160ac
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-14 20:12:52 +0100

    solve day 14

--- haskell/Day14.hs
+++ haskell/Day14.hs
@@ -1,43 +1,25 @@
-import Data.List (transpose, intercalate)
-import Debug.Trace (trace)
+import Data.List (transpose)
 import qualified Data.Map.Strict as M
-import Data.Maybe (fromJust, isJust)
 
 repetitions = 1000000000
 
 main = do
     input <- lines <$> readFile "inputs/14.txt"
-    let width = length input
     print $ calcNorthWeight $ map tilt $ transpose input
-    -- print $ sum $ map (calcWeight (length input)) $ transpose input
-    let until = cycleUntilRepetition (tiltCycle input) M.empty 0
-    let res = last $ take (until + 2) $ iterate tiltCycle input
-    print $ calcNorthWeight $ transpose res
+    print $ getWeightAfterRepetitions input M.empty 1
+
+getWeightAfterRepetitions :: [String] -> M.Map [String] Int -> Int -> Int
+getWeightAfterRepetitions grid seen num = case cycled `M.lookup` seen of
+    Just loopStart -> getNthCycleWeight $ loopStart + (repetitions - loopStart) `mod` (num - loopStart)
+    Nothing        -> getWeightAfterRepetitions cycled (M.insert cycled num seen) (num + 1)
+    where cycled = tiltCycle grid
+          nextSeen = M.insert cycled num seen
+          getNthCycleWeight i = calcNorthWeight $ transpose $ fst $ head $ dropWhile ((/= i) . snd) $ M.assocs nextSeen
 
 calcNorthWeight :: [String] -> Int
-calcNorthWeight grid = sum $ map (calc . zip [len,len-1..]) grid
-    where
-        len = length grid
-        calc :: [(Int, Char)] -> Int
-        calc [] = 0
-        calc ((val, c):xs) = if c == 'O' then val + calc xs else calc xs
-
-calcWeight :: Int -> String -> Int
-calcWeight solid row
-    | null toEnd = currSum
-    | otherwise = currSum + calcWeight nextSolid (tail toEnd)
-    where (until, toEnd) = span (/= '#') row
-          nextSolid = solid - length until - 1
-          rocksAmount = length $ filter (== 'O') until
-          currSum = sum [solid,solid-1..solid-rocksAmount+1]
-
-cycleUntilRepetition :: [String] -> M.Map [String] Int -> Int -> Int
-cycleUntilRepetition grid seen num
-    | isJust val = (repetitions - v) `mod` abs (v - num)
-    | otherwise = cycleUntilRepetition cycled (M.insert cycled num seen) (num + 1)
-    where cycled = tiltCycle grid
-          val = cycled `M.lookup` seen
-          v = fromJust val
+calcNorthWeight grid = sum $ map (calcRow . zip [len,len-1..]) grid
+    where len = length grid
+          calcRow = foldl (\acc (val, c) -> if c == 'O' then val + acc else acc) 0
 
 tiltCycle :: [String] -> [String]
 tiltCycle grid = east
