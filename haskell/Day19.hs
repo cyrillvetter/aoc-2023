0ad27b14d50628aef214c62bd728789637bbed31
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-19 10:57:44 +0100

    day 19 minor renamings

--- haskell/Day19.hs
+++ haskell/Day19.hs
@@ -1,7 +1,6 @@
 import Data.Char (isDigit)
 import Data.List.Split (splitOn, splitOneOf)
 import qualified Data.Map as M
-import Debug.Trace (trace)
 
 data Rule = Condition Char Char Int String | Else String deriving (Show)
 type Workflow = (String, [Rule])
@@ -10,23 +9,23 @@ type Part = (Int, Int, Int, Int)
 main = do
     [workflowsPart, parts] <- map lines . splitOn "\n\n" <$> readFile "inputs/19.txt"
     let workflows = M.fromList $ map parseWorkflow workflowsPart
-        xmasParts = map parseXMAS parts
-    print $ sum $ map (\(x, m, a, s) -> x + m + a + s) $ filter (runWorkflow workflows "in") xmasParts
+        xmasParts = map parsePart parts
+    print $ sum $ map (\(x, m, a, s) -> x + m + a + s) $ filter (isPartAccepted workflows "in") xmasParts
 
-runWorkflow :: M.Map String [Rule] -> String -> Part -> Bool
-runWorkflow _ "A" _ = True
-runWorkflow _ "R" _ = False
-runWorkflow workflows curr part = runWorkflow workflows nextWorkflow part
+isPartAccepted :: M.Map String [Rule] -> String -> Part -> Bool
+isPartAccepted _ "A" _ = True
+isPartAccepted _ "R" _ = False
+isPartAccepted workflows curr part = isPartAccepted workflows nextWorkflow part
     where rules = workflows M.! curr
           nextWorkflow = runRules rules part
 
 runRules :: [Rule] -> Part -> String
 runRules [Else dest] _ = dest
-runRules ((Condition op on val next):rs) part
-    | partVal `operation` val = next
-    | otherwise = runRules rs part
+runRules ((Condition op on val next):cs) part
+    | partVal `ruleOp` val = next
+    | otherwise = runRules cs part
     where partVal = getPartValue part on
-          operation = if op == '<' then (<) else (>)
+          ruleOp = if op == '<' then (<) else (>)
 
 getPartValue :: Part -> Char -> Int
 getPartValue (x, _, _, _) 'x' = x
@@ -46,6 +45,6 @@ parseRule r
     where [cat, num, dest] = splitOneOf "<>:" r
           op = if '<' `elem` r then '<' else '>'
 
-parseXMAS :: String -> Part
-parseXMAS l = (read x, read m, read a, read s)
-    where [x, m, a, s] = map (filter isDigit) $ splitOn "," l
+parsePart :: String -> Part
+parsePart l = (x, m, a, s)
+    where [x, m, a, s] = map (read . filter isDigit) (splitOn "," l)
