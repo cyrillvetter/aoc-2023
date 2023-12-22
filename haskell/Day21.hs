b9699b5176a13a4653dc6dcd81ec64d89194ecb7
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-12-22 23:59:07 +0100

    solve day 22 part 1

---
+++ haskell/Day21.hs
@@ -0,0 +1,30 @@
+import qualified Data.Set as S
+import qualified Data.Array.Unboxed as A
+
+type Point = (Int, Int)
+
+neighbours = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+main = do
+    input <- lines <$> readFile "inputs/21.txt"
+    let bound = length input - 1
+        grid = A.listArray ((0, 0), (bound, bound)) $ concat input
+        start = S.singleton $ fst $ head $ filter ((== 'S') . snd) $ A.assocs grid
+    print $ S.size (iterate (\p -> step (getNeighbours $ S.toList p) grid S.empty) start !! 64)
+
+step :: [Point] -> A.UArray Point Char -> S.Set Point -> S.Set Point
+step [] _ visited = visited
+step (p:ps) grid visited
+    | isOutOfBounds p bound || tile == '#' || p `S.member` visited = step ps grid visited
+    | otherwise = step ps grid (p `S.insert` visited)
+    where (_, (_, bound)) = A.bounds grid
+          tile = grid A.! p
+
+isOutOfBounds :: Point -> Int -> Bool
+isOutOfBounds (x, y) bound = x < 0 && y < 0 && x >= bound && y >= bound
+
+getNeighbours :: [Point] -> [Point]
+getNeighbours = concatMap get
+    where
+        get :: Point -> [Point]
+        get (x, y) = map (\(px, py) -> (px + x, py + y)) neighbours
